27620c969078d73a039ca5828f1df1cb83f5dba9
redByteRus <40762411+redByteRus@users.noreply.github.com>
2020-07-15 11:21:58 +0300

    formatCell()

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,4 +1,5 @@
 #include <Wire.h>
+#include <math.h>
 #include <PZEM004Tv30.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
@@ -6,6 +7,8 @@
 LiquidCrystal_I2C lcd(0x3f, 20, 4); //0x27  0x3f, 20, 4
 
 float w1, w2, w3, v1, v2, v3, a1, a2, a3, wh1, wh2, wh3;
+
+String vS1, aS1, wS1, whS1, vS2, aS2, wS2, whS2, vS3, aS3, wS3, whS3;
 
 PZEM004Tv30 pzem1(2, 16);  // (RX,TX) connect to TX,RX of PZEM
 PZEM004Tv30 pzem2(12, 14); 
@@ -26,6 +29,46 @@ void setup() {
 }
 
 void loop() {
+  updatePzem();
+  Serial.println("pzem1: " + String(v1) + " v " + String(w1) + " w " + "pzem2: " + String(v2) + " v " + String(w2) + " w " + "pzem3: " + String(v3) + " v " + String(w3) + " w");
+  updateDisplay();
+}
+
+void updateDisplay(){
+  //loat test = 220.00;
+
+  //v1 = 220;
+  
+  lcd.clear();
+  lcd.print( formatCell(v1) + "|");
+}
+
+String formatCell(float val) {
+  if (isnan(val)){
+    return "  0.0";
+  } 
+  else if (val <= 0){
+    return "  0.0";
+  } 
+  else if (val < 10){
+    return " " + String(val).substring(0, 4);
+  } 
+  else if (val < 100){
+    return " " + String(val).substring(0, 4);
+  }
+  else if (val < 1000){
+    return String(val).substring(0, 5);
+  }
+  else if (val < 10000){
+    return " " + String(val).substring(0, 4);
+  }
+  else if (val < 100000){
+    return " " + String(val).substring(0, 4);
+  }
+  return String(val);
+}
+
+void updatePzem(){
   v1 = pzem1.voltage();
   v2 = pzem2.voltage();
   v3 = pzem3.voltage();
@@ -41,85 +84,4 @@ void loop() {
   wh1 = pzem1.energy();
   wh2 = pzem2.energy();
   wh3 = pzem3.energy();
-
-  Serial.println("pzem1: " + String(v1) + " v " + String(w1) + " w " + "pzem2: " + String(v2) + " v " + String(w2) + " w " + "pzem3: " + String(v3) + " v " + String(w3) + " w");
-
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Ln   V    A     W    ");
-  lcd.setCursor(0,1);
-  
-  //if (v1 > 100 && a1 < 10 && w1 > 100 && w1 < 999){
-    
-  if ( v1 > 0.0 ){
-    String Ad1 = " 0 ";
-    String Wd1 = "0";
-  
-    if ( a1 < 10 ){
-      Ad1 = String( a1 ).substring(0,3); 
-    } else {
-      Ad1 = String( a1 ).substring(0,4);
-    }
-
-    if (w1 <= 0) { 
-      Wd1 = "  0";
-    } else {
-      Wd1 = String( int( w1 ) );
-    }
-  
-    lcd.print("1   " + String(v1).substring(0, 3)  + "  " + Ad1 + "  " + Wd1 + "      " );
-  }
-  else {
-    lcd.print("1    0    0     0    ");
-  }
-
-  lcd.setCursor(0,2);
-
-  if ( v2 > 0.0 ){
-    String Ad2 = " 0 ";
-    String Wd2 = "0";
-  
-    if ( a2 < 10 ){
-      Ad2 = String( a2 ).substring(0,3); 
-    } else {
-      Ad2 = String( a2 ).substring(0,4);
-    }
-
-    if (w2 <= 0) { 
-      Wd2 = "  0";
-    } else {
-      Wd2 = String( int( w2 ) );
-    }
-  
-    lcd.print("2   " + String(v2).substring(0, 3)  + "  " + Ad2 + "  " + Wd2 + "      " );
-  }
-  else {
-    lcd.print("2    0    0     0    ");
-  }
-
-  lcd.setCursor(0,3);
-
-  if ( v3 > 0.0 ){
-    String Ad3 = " 0 ";
-    String Wd3 = "0";
-  
-    if ( a3 < 10 ){
-      Ad3 = String( a3 ).substring(0,3); 
-    } else {
-      Ad3 = String( a3 ).substring(0,4);
-    }
-
-    if (w3 <= 0) { 
-      Wd3 = "  0";
-    } else {
-      Wd3 = String( int( w3 ) );
-    }
-  
-    lcd.print("3   " + String(v3).substring(0, 3)  + "  " + Ad3 + "  " + Wd3 + "      " );
-  }
-  else {
-    lcd.print("3    0    0     0    ");
-  }
-  
-  lcd.setCursor(0,0);
 }
