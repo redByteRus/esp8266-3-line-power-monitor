722f324ac1dfc6eaf08f7962ab7867c5e52afda6
redByteRus <40762411+redByteRus@users.noreply.github.com>
2020-07-16 11:18:23 +0300

    Отображение no power

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -7,6 +7,8 @@ LiquidCrystal_I2C lcd(0x3f, 20, 4); //0x27  0x3f, 20, 4
 
 float w1, w2, w3, v1, v2, v3, a1, a2, a3, wh1, wh2, wh3;
 
+bool power1, power2, power3 ;
+
 PZEM004Tv30 pzem1(2, 16);  // (RX,TX) connect to TX,RX of PZEM
 PZEM004Tv30 pzem2(12, 14); 
 PZEM004Tv30 pzem3(D3, D7); 
@@ -33,34 +35,89 @@ void loop() {
 
 void updateDisplay(){
   String displayStr = "";
+  if (power1){
+    displayStr += (formatCell(v1) + " ");
+  }else{
+    displayStr += "       ";
+  }
 
-  displayStr += (formatCell(v1) + " ");
-  displayStr += (formatCell(v2) + " ");
-  displayStr += (formatCell(v3));
+  if (power2){
+    displayStr += (formatCell(v2) + " ");
+  }else{
+    displayStr += "       ";
+  }
+
+  if (power3){
+    displayStr += (formatCell(v3));
+  }else{
+    displayStr += "      ";
+  }
 
   lcd.setCursor(0,0);
   lcd.print(displayStr);
   displayStr = "";
 
-  displayStr += (formatCell(a1) + " ");
-  displayStr += (formatCell(a2) + " ");
-  displayStr += (formatCell(a3));
+  if (power1){
+    displayStr += (formatCell(a1) + " ");
+  }else{
+    displayStr += "  NO   ";
+  }
+
+  if (power2){
+    displayStr += (formatCell(a2) + " ");
+  }else{
+    displayStr += "  NO   ";
+  }
+
+  if (power3){
+    displayStr += (formatCell(a3));
+  }else{
+    displayStr += "  NO  ";
+  }
 
   lcd.setCursor(0,1);
   lcd.print(displayStr);
   displayStr = "";
   
-  displayStr += (formatCell(w1) + " ");
-  displayStr += (formatCell(w2) + " ");
-  displayStr += (formatCell(w3));
+  if (power1){
+    displayStr += (formatCell(w1) + " ");
+  }else{
+    displayStr += " POWER ";
+  }
+
+  if (power2){
+    displayStr += (formatCell(w2) + " ");
+  }else{
+    displayStr += " POWER ";
+  }
+
+  if (power3){
+    displayStr += (formatCell(w3));
+  }else{
+    displayStr += " POWER";
+  }
 
   lcd.setCursor(0,2);
   lcd.print(displayStr);
   displayStr = "";
 
-  displayStr += (formatCell(wh1) + " ");
-  displayStr += (formatCell(wh2) + " ");
-  displayStr += (formatCell(wh3));
+  if (power1){
+    displayStr += (formatCell(wh1) + " ");
+  }else{
+    displayStr += "       ";
+  }
+
+  if (power2){
+    displayStr += (formatCell(wh2) + " ");
+  }else{
+    displayStr += "       ";
+  }
+
+  if (power3){
+    displayStr += (formatCell(wh3));
+  }else{
+    displayStr += "      ";
+  }
 
   lcd.setCursor(0,3);
   lcd.print(displayStr);
@@ -96,6 +153,24 @@ void updatePzem(){
   v1 = pzem1.voltage();
   v2 = pzem2.voltage();
   v3 = pzem3.voltage();
+
+  if(isnan(v1)){
+    power1 = false;
+  } else {
+    power1 = true;
+  }
+
+  if(isnan(v2)){
+    power2 = false;
+  } else {
+    power2 = true;
+  }
+
+  if(isnan(v3)){
+    power3 = false;
+  } else {
+    power3 = true;
+  }
 
   a1 = pzem1.current();
   a2 = pzem2.current();
